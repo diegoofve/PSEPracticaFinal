4d954f1a313effef3994562700c81faa5c7be662
Raúl <raul.fernandez23@estudiantes.uva.es>
2026-05-08 19:00:25 +0200

    prisma schema + setup index to work

---
+++ servidor/middlewares/rutasprivadas.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const routerPrivado = Router();
+
+export default routerPrivado;
---
+++ servidor/middlewares/rutaspublicas.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const routerPublico = Router();
+
+export default routerPublico;
